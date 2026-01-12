b08ef3c1142dad2bf2e80277221814ec472f576f
HelenSolS <Elena.soldatova@gmail.com>
2026-01-12 12:44:12 +0300

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'process.env': process.env
+  },
+  server: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    }
+  }
+});
